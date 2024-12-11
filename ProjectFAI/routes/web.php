063c93b2852b8751753562aa10d4db8a85ab57ff
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-12-11 20:41:21 +0700

    #5

--- ProjectFAI/routes/web.php
+++ ProjectFAI/routes/web.php
@@ -2,16 +2,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login'); 
+    return view('login');
 });
 
 Route::post('/login', function () {
- 
-    return redirect('/index'); 
+
+    return redirect('/index');
 });
 
 Route::get('/index', function () {
-    return view('index'); 
+    return view('index');
 });
 
 Route::get('/menu', function () {
@@ -20,5 +20,8 @@ Route::get('/menu', function () {
 
 Route::get('/payment', function () {
     return view('payment');
+});
+Route::get('/admin', function () {
+    return view('admin');
 });
 
