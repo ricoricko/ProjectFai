5fbbf6262668274d1877ad8df7f6ca696a389027
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-12-12 11:56:08 +0700

    #5 Front end admin

--- ProjectFAI/routes/web.php
+++ ProjectFAI/routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,3 +26,7 @@ Route::get('/payment', function () {
 Route::get('/admin', function () {
     return view('admin');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
