e587cafb70addd50ab0f7960ed4642396ede22ac
ricoricko <ricofrede20@gmail.com>
2024-12-30 12:12:30 +0700

    #17 Middleware admin
    Middleware admin

--- ProjectFAI/routes/web.php
+++ ProjectFAI/routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\CartController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return view('login');
@@ -27,24 +28,30 @@ Route::get('/payment', function () {
     return view('payment');
 })->name('payment');
 
-Route::get('/admin', function () {
-    return view('admin');
+
+
+Route::middleware([AdminMiddleware::class])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin');
+    });
+    Route::get('/admin/produk', [ManagerController::class, 'indexproduk'])->name('admin.produk');
+    Route::post('/admin/produk/add', [ManagerController::class, 'addproduk'])->name('admin.addproduk');
+    Route::put('/admin/produk/update/{id}', [ManagerController::class, 'updateProduk'])->name('admin.updateproduk');
+    Route::delete('/admin/produk/delete/{id}', [ManagerController::class, 'deleteProduk'])->name('admin.deleteproduk');
+    
+    
+    
+    Route::get('/admin/kategori', [ManagerController::class, 'indexkategori'])->name('admin.kategori');
+    Route::post('/admin/kategori/add', [ManagerController::class, 'addkategori'])->name('admin.addkategori');
+    Route::put('/admin/kategori/update/{id}', [ManagerController::class, 'updateKategori'])->name('admin.updatekategori');
+    Route::delete('/admin/kategori/delete/{id}', [ManagerController::class, 'deleteKategori'])->name('admin.deletekategori');
+    
+    Route::get('/admin/users', [ManagerController::class, 'indexUsers'])->name('admin.users');
+    Route::get('/admin/menu', [ManagerController::class, 'indexMenu'])->name('admin.menu');
+    Route::post('/admin-addMenu', [ManagerController::class, 'addMenu'])->name('admin.addMenu');
 });
-Route::get('/admin/produk', [ManagerController::class, 'indexproduk'])->name('admin.produk');
-Route::post('/admin/produk/add', [ManagerController::class, 'addproduk'])->name('admin.addproduk');
-Route::put('/admin/produk/update/{id}', [ManagerController::class, 'updateProduk'])->name('admin.updateproduk');
-Route::delete('/admin/produk/delete/{id}', [ManagerController::class, 'deleteProduk'])->name('admin.deleteproduk');
 
 
-
-Route::get('/admin/kategori', [ManagerController::class, 'indexkategori'])->name('admin.kategori');
-Route::post('/admin/kategori/add', [ManagerController::class, 'addkategori'])->name('admin.addkategori');
-Route::put('/admin/kategori/update/{id}', [ManagerController::class, 'updateKategori'])->name('admin.updatekategori');
-Route::delete('/admin/kategori/delete/{id}', [ManagerController::class, 'deleteKategori'])->name('admin.deletekategori');
-
-Route::get('/admin/users', [ManagerController::class, 'indexUsers'])->name('admin.users');
-Route::get('/admin/menu', [ManagerController::class, 'indexMenu'])->name('admin.menu');
-Route::post('/admin-addMenu', [ManagerController::class, 'addMenu'])->name('admin.addMenu');
 
 Auth::routes();
 
@@ -57,8 +64,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
 
-Route::get('/admin', [ManagerController::class, 'index'])->name('admin.index');
-Route::resource('admin', ManagerController::class);
+Route::middleware([AdminMiddleware::class])->group(function () {
+    Route::get('/admin', [ManagerController::class, 'index'])->name('admin.index');
+    Route::resource('admin', ManagerController::class);
+});
+
 
 Route::get('/admin/create', [ManagerController::class, 'create'])->name('admin.create');
 Route::post('/admin', [ManagerController::class, 'store'])->name('admin.store');
